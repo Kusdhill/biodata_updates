bfbeffe3bb5c8a06c144359de5fddd4f65543284
UR Admin <kusdhill@ucsc.edu>
2017-07-12 15:17:21 -0700

    file checking

--- parse.py
+++ parse.py
@@ -1,7 +1,40 @@
+import sys
+import os
 import openpyxl
 
+# verifies that a given filename has .xlsx extension
+# if it does not, an error is thrown
+def check_extension(filename):
+	extension_flag = 0
+	extension = ""
+	good_extension = "xlsx"
+
+	for char in filename:
+		if extension_flag==0:
+			if char==".":
+				extension_flag = 1
+		else:
+			extension+=char
+
+	if extension!=good_extension:
+		sys.exit("must pass in .xlsx files")
+
+
+# verifies that file exists
+# if it does not, an error is thrown
+def check_existence(filename):
+	if not os.path.isfile(filename):
+		sys.exit("file must exist")
+
 def main():
-	print("hello world")
+	print("checking command line arguments")
+	if len(sys.argv)!=2:
+		sys.exit("usage: python parse.py filename.xlsx")
+	else:
+		print("verifying file extension")
+		check_extension(sys.argv[1])
+		print("verifying existence")
+		check_existence(sys.argv[1])
 
 if __name__ == '__main__':
 	main()
