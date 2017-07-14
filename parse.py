110422467544b6ec9021c7070468c647233a8bd9
UR Admin <kusdhill@ucsc.edu>
2017-07-14 10:48:34 -0700

    xls conversion

--- parse.py
+++ parse.py
@@ -1,6 +1,9 @@
 import sys
 import os
-import openpyxl
+import subprocess
+import openpyxl as xl
+import xlrd as x
+
 
 # verifies that a given filename has .xlsx extension
 # if it does not, an error is thrown
@@ -26,6 +29,29 @@ def check_existence(filename):
 	if not os.path.isfile(filename):
 		sys.exit("file must exist")
 
+def convert_xls(filename):
+	xls_filename = filename[0:-4]+"xls"
+	subprocess.call(["ssconvert", "--export-type=Gnumeric_Excel:excel_dsf", filename, xls_filename])
+	return(xls_filename)
+
+
+#ssconvert --export-type="Gnumeric_Excel:excel_dsf"
+
+def parse_xlsx(filename):
+	book = x.open_workbook(filename, formatting_info=True)
+	for sheet in book.sheets():
+		sheet_name = sheet.name.lower()
+		print(sheet_name)
+		if "debrief" in sheet_name:
+			debrief_sheet = sheet_name
+
+	if not 'debrief_sheet' in locals():
+		sys.exit("no debrief sheet found in .xlsx file")
+	else:
+		print("found debrief sheet")
+
+
+
 def main():
 	print("checking command line arguments")
 	if len(sys.argv)!=2:
@@ -35,6 +61,14 @@ def main():
 		check_extension(sys.argv[1])
 		print("verifying existence")
 		check_existence(sys.argv[1])
+		filename = sys.argv[1]
+		print("converting to xls")
+		xls_filename = convert_xls(filename)
+		print("parsing xls")
+		parse_xlsx(xls_filename)
+
+
+	
 
 if __name__ == '__main__':
 	main()
